50ebde329199100120299b0126a626ee0d6dac3e
Muroko <murokomail@gmail.com>
2024-03-05 20:24:14 -0500

    CHANGE: contracts config

--- front-end/src/utils/contracts-config.js
+++ front-end/src/utils/contracts-config.js
@@ -1,11 +1,11 @@
 
       // Contracts Deployed And Verified On Dnero Mainnet
-      //DGNMMarket
-      export const marketContractAddress = "0x80f17f2FA3e9E24Aa0772Ac715F547C22ddfeB71"
-      //DGNMCollectors
-      export const collectorsContractAddress = "0x1Dae253706b7600D8D657b55FB033ae10d018Ad3"
-      //DGNMCollection
-      export const nftContractAddress = "0xca38076a23666FcDF544F473e41AA76B981289BF"
+      //DGNMMarket-3rd
+      export const marketContractAddress = "0x72948034ee6f41ad08adab214ef0709a27ef278d"
+      //DGNMCollectors-1st
+      export const collectorsContractAddress = "0xdf5c039871353c9317babd63eb426c727b6727dd"
+      //DGNMCollection-2nd
+      export const nftContractAddress = "0x5dee317aadde6597d6f955fa88da5492c69a7945"
       //
       export const mockDAIAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
       export const ownerAddress = "0x32943D8EDAdD1f202e17115Ef977C9AFcc464c0e"
